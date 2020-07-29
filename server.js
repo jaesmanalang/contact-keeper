f549754f6093d8a5d4ad6577cf676659725dd594
jaesmanalang <jaesmanalang@gmail.com>
2020-07-29 13:27:24 +0800

    Removed initial route

--- server.js
+++ server.js
@@ -12,8 +12,6 @@ app.use(express.json({ extended: false }));
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => res.json({ msg: 'Welcome to contact keeper API' }));
-
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
